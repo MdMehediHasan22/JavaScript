e967468b0a9be6e39ea6d60d9684deea14ebe3cc
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-10-10 17:55:04 +0600

    array

---
+++ array.js
@@ -0,0 +1,20 @@
+var student = [22,55,43,7,2,9];
+var nameStudent = ['Rahim','Karim','Jasim','Miraj'];
+console.log(nameStudent);
+console.log(nameStudent.length);
+console.log(student);
+console.log(student.length);
+
+// index
+var element1 =student[0];
+console.log(element1);
+var noOfStudent = nameStudent[3];
+console.log(noOfStudent);
+
+// set
+student[2]=12;
+console.log(student);
+// find index of an element
+
+var positionIndex =student.indexOf(2);
+console.log(positionIndex);
