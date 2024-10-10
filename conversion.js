469cdaaca6454b3031f734b06b5b549d535fc32e
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-10-10 17:32:31 +0600

    introduction

---
+++ conversion.js
@@ -0,0 +1,18 @@
+var gpa='3.80';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
+var price=10;
+console.log(typeof price);
+var price='100';
+var isHappy = true;
+
+
+console.log(typeof price);
+console.log(typeof isHappy);
+
+var price1=.2;
+var price2=.1;
+var sum=price1+price2;
+sum=sum.toFixed(2);
+console.log(sum);
